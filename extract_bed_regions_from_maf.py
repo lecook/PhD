c7a3a8c4382d70c9628af037afbc56381bf1ce00
Laura Cook <lecook@student.unimelb.edu.au>
2019-02-12 12:16:26 +1100

    updated

--- extract_bed_regions_from_maf.py
+++ extract_bed_regions_from_maf.py
@@ -1,5 +1,19 @@
 #!usr/bin/env python3
 
+#Note, maf_parse assumes that each input maf alignment is against
+#a single reference scaffold. In other words, you don't have any
+#way to tell it to search against mouse chromosome 10 because
+#the file should only contain alignments against mouse chromosome 10
+
+#After running, use these sed commands to remove unwanted lines
+
+#sed -i '/#eof/d' [myfiles].maf
+#sed -i '/##maf version/d' [myfiles].maf
+#sed -i '/maf_parse/d' [myfiles].maf
+
+#And run this to add a header line to the file
+#sed -i '1s/^/##maf version=1 scoring=roast.v3.3\n/' [myfiles].maf
+
 import sys
 import os
 
@@ -16,9 +30,9 @@ with open(bed_regions, 'r') as inputfile:
 
 # Iterate through bed file line-by-line
 # without loading the whole file into the program.
-	for line in inputfile:
+    for line in inputfile:
 
-  # split the bed region at whitespace (i.e. tabs)
+        # split the bed region at whitespace (i.e. tabs)
         line = line.strip("\n")
         region = line.split("\t")
         print(region)
@@ -38,3 +52,6 @@ with open(bed_regions, 'r') as inputfile:
 
         # run the command
         os.system(bashCommand)
+
+
+
